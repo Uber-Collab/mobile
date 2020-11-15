bb4e7fdc343d859d51eb7f2e04863f31f9d7b4fd
gabrielmarques5 <gabriel.alves.marques@hotmail.com>
2020-11-15 18:18:44 -0300

    first commit

--- Mobile/src/pages/LoginUberCollab/index.tsx
+++ Mobile/src/pages/LoginUberCollab/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 export default function LoginUberCollab() {
     return (
         <View style={styles.container}>
-            <Text>
-                Menos trânsito 
-                Mais mobilidade 
+            <Text style={styles.title}>
+                Menos trânsito {"\n"}
+                Mais mobilidade {"\n"}
                 Menos stress
             </Text>
+
+            <Text>CPF</Text>
+            <TextInput style={styles.insertCpf}></TextInput>
         </View>
     )
 }
@@ -16,5 +19,15 @@ export default function LoginUberCollab() {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+
+    title: {
+        fontSize: 32,
+        padding: 25,
+        top: 70
+    },
+
+    insertCpf: {
+        borderColor: '#f0f0f0',
     }
 })
