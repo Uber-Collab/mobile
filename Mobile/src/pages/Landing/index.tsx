cae782557a4a9740d639d74a4942278275984bfb
gabrielmarques5 <gabriel.alves.marques@hotmail.com>
2020-11-15 02:39:30 -0300

    Iniciando o banner

--- Mobile/src/pages/Landing/index.tsx
+++ Mobile/src/pages/Landing/index.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
 
-import landingImg from '../../images/Banner.png';
+import bannerImg from '../../images/Banner.png';
+import buildingImg from '../../images/Predio.png';
 
 export default function Landing() {
     return (
         <View style={styles.container}>
-            <Text>Hello</Text>
+            <View style={styles.bannerContainer}>
+                <Image source={bannerImg} style={styles.banner} />
+                <Image source={buildingImg} style={styles.building} />
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff'
     },
-});
-  
+
+    bannerContainer: {
+        flexDirection: 'row',
+    },
+
+    banner: {
+        width: '100%'
+    },
+
+    building: {
+        top: 170,
+        right: 130,
+    }
+})
